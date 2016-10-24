d7eeca4e04175dd19b8e31d84d67c92f0283af65
hiimnit <blacksnightmare@gmail.com>
2016-10-24 13:28:19 +0200

    v1.0.1

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    int n;
+    if(argc != 2) {
+        n = 1000000;
+    } else {
+        n = atoi(argv[1]);
+        n = abs(n);
+    }
+    cout << "#points : " << n << endl;
+
     randomizePoints * rP = new randomizePoints();
-    vector<Point *> * points = rP->getPoints(10000);
+    vector<Point *> * points = rP->getPoints(n);
 
     // cast pro mereni
     quickHull * qH = new quickHull(*points);
@@ -16,7 +25,7 @@ int main() {
     // testloop
     int lastdir = -1;
     for(auto e : resultEdges) {
-        //e->Print();
+        // e->Print();
 
         if (lastdir > e->direction()) {
             cout << "Err" << endl;
@@ -25,7 +34,7 @@ int main() {
         lastdir = e->direction();
     }
 
-    cout << "ding" << endl;
+    cout << "done" << endl;
 
     for(size_t i = 0; i < points->size(); ++i) delete points->at(i);
     delete rP;
--- randomizePoints.cpp
+++ randomizePoints.cpp
@@ -20,21 +20,20 @@ vector<Point *> * randomizePoints::getPoints(int n) {
         resultSet->insert(new Point(-1, -1));
         resultSet->insert(new Point(1, -1));
         resultSet->insert(new Point(2, -1));
-    } else if (n == 1) {
-        resultSet->insert(new Point(-4, 0));
-        resultSet->insert(new Point(-3, 3));
-        resultSet->insert(new Point(-2, 2));
-        resultSet->insert(new Point(-1, 1));
-        resultSet->insert(new Point(0, 4));
-
     } else {
         srand (time(NULL));
-        while (resultSet->size() != (unsigned int)(10 * n)) {
-            resultSet->insert(new Point(rand() % n - n/2, rand() % n - n/2));
+        int x = 50000;
+        cout << "grid : <-" << x/2 << ", " << x/2 << ">" << endl;
+        while (resultSet->size() != (unsigned int)n) {
+            Point * p = new Point(rand() % x - x/2, rand() % x - x/2);
+            pair<set<Point *>::iterator, bool> resPair;
+            resPair = resultSet->insert(p);
+            if(!resPair.second) delete p;
         }
     }
 
     res->assign(resultSet->begin(), resultSet->end());
+    cout << "points generated" << endl;
     return res;
 }
 
